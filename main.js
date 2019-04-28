c50c9b6ca600e5b9a9f95027a6f5465617bcdc86
Andrea Baldocchi <andrea.baldocchi21@gmail.com>
2019-04-28 17:19:14 +0200

    fine

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 // Fizz Buzz test: Scrivi un programma che stampi i numeri da 1 a 100 ma per i multipli di 3
 //  stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-//  Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+//  Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz, mostrarlo su pagina
 
 var i = 0;
 var j = 100;
 var num = 0;
+var soluzione = document.querySelector('#numeri');
 
 while (i < j) {
   num = i + 1;
-   if ((num % 5 == 0) && (num % 3 == 0)) {
-     console.log("FizzBuzz");
-     // document.getElementById('numeri').innerHTML= "FizzBuzz";
-   } else if (num % 5 == 0){
-     console.log("Buzz");
-     // document.getElementById('numeri').innerHTML= "Buzz";
-   } else if (num % 3 == 0) {
-     console.log("Fizz");
-     // document.getElementById('numeri').innerHTML= "Fizz";
-   } else {
-     console.log(num);
-     // document.getElementById('numeri').innerHTML= num;
-   }
+
+  if ((num % 5 == 0) && (num % 3 == 0)) {
+    // console.log("FizzBuzz");
+    soluzione.insertAdjacentText('beforeend',' FizzBuzz');
+    soluzione.insertAdjacentHTML('beforeend', '<div class = "spazio"> </div>');
+  } else if (num % 5 == 0){
+    // console.log("Buzz");
+    soluzione.insertAdjacentText('beforeend',' Buzz');
+    soluzione.insertAdjacentHTML('beforeend', '<div class = "spazio"> </div>');
+  } else if (num % 3 == 0) {
+    // console.log("Fizz");
+    soluzione.insertAdjacentText('beforeend',' Fizz');
+    soluzione.insertAdjacentHTML('beforeend', '<div class = "spazio"> </div>');
+  } else {
+    // console.log(num);
+    soluzione.insertAdjacentText('beforeend',' ' + num);
+    soluzione.insertAdjacentHTML('beforeend', '<div class = "spazio"> </div>');
+  }
+
   i++;
 }
